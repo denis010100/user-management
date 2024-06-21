2cbc8c1cd019e7dc8b9f0b71979c3f0bc500d8c9
denisLazarus <denis@sourcelab.bg>
2024-06-21 16:30:05 +0300

    feat: implemented main and posts pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,237 @@
-import Image from "next/image";
+"use client"
+
+import axios from "axios"
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query"
+import type { CollapseProps } from "antd"
+import { Button, Collapse, Form, Input, message } from "antd"
+import { useState } from "react"
+import Link from "next/link"
+
+const fetchUsers = async () => {
+  const { data } = await axios.get("https://jsonplaceholder.typicode.com/users")
+  return data.slice(0, 10)
+}
+
+const updateUser = async ({ id, data }: { id: string; data: any }) => {
+  const transformedData = {
+    ...data,
+    address: {
+      street: data.street,
+      suite: data.suite,
+      city: data.city,
+    },
+  }
+  delete transformedData.street
+  delete transformedData.suite
+  delete transformedData.city
+
+  const response = await axios.patch(
+    `https://jsonplaceholder.typicode.com/users/${id}`,
+    data
+  )
+  return response.data
+}
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const [isFormChanged, setIsFormChanged] = useState(false)
+  const [currentUser, setCurrentUser] = useState(null)
+  const [activeKey, setActiveKey] = useState<string | string[]>("")
+
+  const [form] = Form.useForm()
+  const queryClient = useQueryClient()
+
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["users"],
+    queryFn: fetchUsers,
+  })
+
+  const mutation = useMutation({
+    mutationFn: updateUser,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["users"] })
+      message.success("User updated successfully")
+      setIsFormChanged(false)
+    },
+    onError: (error: any) => {
+      message.error(`Error: ${error.message}`)
+    },
+  })
+
+  const handleCollapseChange = (key: string | string[]) => {
+    setActiveKey(key[0])
+    const user = data?.find((u: any) => u.id.toString() === key[0])
+
+    if (user) {
+      setCurrentUser(user)
+      form.resetFields()
+      form.setFieldsValue(user)
+      setIsFormChanged(false)
+    }
+  }
+
+  const handleFormChange = (changedValues: any, allValues: any) => {
+    if (currentUser) {
+      const isChanged = Object.keys(changedValues).some(
+        (key) => changedValues[key] !== currentUser[key]
+      )
+      setIsFormChanged(isChanged)
+    }
+  }
+
+  const handleFormSubmit = (values: any) => {
+    if (currentUser) {
+      mutation.mutate({ id: currentUser, data: values })
+    }
+  }
+
+  const items: CollapseProps["items"] = data?.map((user: any) => ({
+    key: user.id.toString(),
+    label: user.name,
+    children: (
+      <div className="flex flex-col gap-4 text-lg">
+        <Form
+          style={{ width: "30%" }}
+          onValuesChange={handleFormChange}
+          onFinish={handleFormSubmit}
+          layout="vertical"
+          form={form}
+        >
+          <Form.Item label="Name" name="name">
+            <Input />
+          </Form.Item>
+          <Form.Item
+            rules={[
+              {
+                required: true,
+                message: "Please input your email!",
+              },
+              {
+                pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                message: "Please enter a valid email",
+              },
+            ]}
+            label="Email"
+            name="email"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <Input />
+          </Form.Item>
+          <Form.Item
+            tooltip={
+              "Formats are as follows 123-456-7890 (123) 456-7890 123 456 7890 123.456.7890 +91 (123) 456-7890"
+            }
+            rules={[
+              {
+                pattern: /^(\+\d{1,2}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/,
+                message: "Please enter a valid phone number",
+              },
+            ]}
+            label="Phone"
+            name="phone"
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item label="Website" name="website">
+            <Input />
+          </Form.Item>
+          <Form.Item
+            rules={[
+              {
+                required: true,
+                message: "Please input your username!",
+              },
+            ]}
+            label="Username"
+            name="username"
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item label="Website" name="website">
+            <Input />
+          </Form.Item>
+          <Form.Item
+            initialValue={user.address.street}
+            rules={[
+              {
+                required: true,
+                message: "Please input your address street!",
+              },
+            ]}
+            label="Address Street"
+            name={["street"]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            initialValue={user.address.suite}
+            rules={[
+              {
+                required: true,
+                message: "Please input your address suite!",
+              },
+            ]}
+            label="Address Suite"
+            name={["suite"]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            initialValue={user.address.city}
+            rules={[
+              {
+                required: true,
+                message: "Please input your address city!",
+              },
+            ]}
+            label="Address City"
+            name={["city"]}
+          >
+            <Input />
+          </Form.Item>
+          <div className="flex flex-row gap-6">
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                disabled={!isFormChanged}
+              >
+                Submit
+              </Button>
+            </Form.Item>
+            <Form.Item>
+              <Button
+                onClick={() => {
+                  form.resetFields()
+                  form.setFieldsValue(currentUser)
+                  setIsFormChanged(false)
+                }}
+                danger
+                disabled={!isFormChanged}
+              >
+                Cancel
+              </Button>
+            </Form.Item>
+          </div>
+        </Form>
+        <div>
+          <Link href={`user/${user.id}/posts`}>
+            <Button>See posts 🔎</Button>
+          </Link>
         </div>
       </div>
+    ),
+  }))
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+  if (isLoading) return <div>Loading...</div>
+  if (error) return <div>An error occurred: {error.message}</div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+  return (
+    <div>
+      <Collapse
+        activeKey={activeKey}
+        onChange={handleCollapseChange}
+        accordion
+        items={items}
+      />
+    </div>
+  )
 }
---
+++ src/app/user/[slug]/posts/page.tsx
@@ -0,0 +1,393 @@
+"use client"
+
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+import {
+  Button,
+  Collapse,
+  CollapseProps,
+  Form,
+  Input,
+  Modal,
+  message,
+} from "antd"
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+const updateUser = async ({ id, data }: { id: string; data: any }) => {
+  const transformedData = {
+    ...data,
+    address: {
+      street: data.street,
+      suite: data.suite,
+      city: data.city,
+    },
+  }
+  delete transformedData.street
+  delete transformedData.suite
+  delete transformedData.city
+
+  const response = await axios.patch(
+    `https://jsonplaceholder.typicode.com/users/${id}`,
+    transformedData
+  )
+  return response.data
+}
+
+const deletePost = async ({ id }: { id: string }) => {
+  const response = await axios.delete(
+    `https://jsonplaceholder.typicode.com/posts/${id}`
+  )
+  return response.data
+}
+
+export default function Page({
+  params: { slug },
+}: {
+  params: { slug: string }
+}) {
+  const fetchUser = async () => {
+    const { data } = await axios.get(
+      `https://jsonplaceholder.typicode.com/users/${slug}`
+    )
+    return data
+  }
+
+  const fetchUserPosts = async () => {
+    const { data } = await axios.get(
+      `https://jsonplaceholder.typicode.com/users/${slug}/posts`
+    )
+    return data
+  }
+
+  const [form] = Form.useForm()
+  const [postForm] = Form.useForm()
+  const [isFormChanged, setIsFormChanged] = useState(false)
+  const [isPostFormChanged, setIsPostFormChanged] = useState(false)
+  const queryClient = useQueryClient()
+  const [activeKey, setActiveKey] = useState<string | string[]>("")
+  const [currentPost, setCurrentPost] = useState(null)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [localUserPosts, setLocalUserPosts] = useState([])
+
+  const handleCancel = () => {
+    setIsModalOpen(false)
+  }
+
+  const {
+    data: userData,
+    error: userError,
+    isLoading: userLoading,
+  } = useQuery({
+    queryKey: ["users", slug],
+    queryFn: fetchUser,
+  })
+
+  const {
+    data: userPosts,
+    error: postsError,
+    isLoading: postsLoading,
+  } = useQuery({
+    queryKey: ["userPosts", slug],
+    queryFn: fetchUserPosts,
+  })
+
+  useEffect(() => {
+    if (userPosts) {
+      setLocalUserPosts(userPosts)
+    }
+  }, [userPosts])
+
+  const userMutation = useMutation({
+    mutationFn: updateUser,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["users", slug] })
+      message.success("User updated successfully")
+      setIsFormChanged(false)
+    },
+    onError: (error) => {
+      message.error(`Error: ${error.message}`)
+    },
+  })
+
+  const postDeleteMutation = useMutation({
+    mutationFn: deletePost,
+    onSuccess: (data, variables) => {
+      setLocalUserPosts((prevPosts) =>
+        prevPosts.filter((post: any) => post.id.toString() !== variables.id)
+      )
+      message.success("Post deleted successfully")
+      setIsModalOpen(false)
+    },
+    onError: (error) => {
+      message.error(`Error: ${error.message}`)
+    },
+  })
+
+  useEffect(() => {
+    form.setFieldsValue(userData)
+  }, [userData, form])
+
+  const handleFormChange = (changedValues: any) => {
+    if (userData) {
+      const isChanged = Object.keys(changedValues).some((key) => {
+        if (key === "street" || key === "suite" || key === "city") {
+          return changedValues[key] !== userData.address[key]
+        }
+        return changedValues[key] !== userData[key]
+      })
+      setIsFormChanged(isChanged)
+    }
+  }
+
+  const handlePostFormChange = (changedValues: any) => {
+    if (currentPost) {
+      const isChanged = Object.keys(changedValues).some((key) => {
+        return changedValues[key] !== currentPost[key]
+      })
+      setIsPostFormChanged(isChanged)
+    }
+  }
+
+  const handleFormSubmit = (values: any) => {
+    if (userData) {
+      const transformedValues = {
+        ...values,
+        street: values.address.street,
+        suite: values.address.suite,
+        city: values.address.city,
+      }
+      delete transformedValues.address
+      userMutation.mutate({ id: userData.id, data: transformedValues })
+    }
+  }
+
+  const handlePostFormSubmit = (values: any) => {
+    if (currentPost) {
+      userMutation.mutate({ id: userData.id, data: values })
+    }
+  }
+
+  const handleCollapseChange = (key: string | string[]) => {
+    setActiveKey(key[0])
+    const post = localUserPosts?.find((u: any) => u.id.toString() === key[0])
+
+    if (post) {
+      setCurrentPost(post)
+      postForm.resetFields()
+      postForm.setFieldsValue(post)
+      setIsPostFormChanged(false)
+    }
+  }
+
+  const items: CollapseProps["items"] = localUserPosts?.map((post: any) => ({
+    key: post.id.toString(),
+    label: post.title,
+    children: (
+      <div className="flex flex-col gap-4 text-lg">
+        <Form
+          style={{ width: "100%" }}
+          onValuesChange={handlePostFormChange}
+          onFinish={handlePostFormSubmit}
+          layout="vertical"
+          form={postForm}
+        >
+          <Form.Item label="Title" name="title">
+            <Input />
+          </Form.Item>
+
+          <Form.Item label="Body" name="body">
+            <Input.TextArea />
+          </Form.Item>
+          <div className="flex flex-row gap-6">
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                disabled={!isPostFormChanged}
+              >
+                Submit
+              </Button>
+            </Form.Item>
+            <Form.Item>
+              <Button
+                onClick={() => {
+                  postForm.resetFields()
+                  postForm.setFieldsValue(localUserPosts)
+                  setIsPostFormChanged(false)
+                }}
+                danger
+                disabled={!isFormChanged}
+              >
+                Cancel
+              </Button>
+            </Form.Item>
+            <Form.Item>
+              <Button onClick={() => setIsModalOpen(true)} danger>
+                Delete
+              </Button>
+              <Modal
+                title="Delete Post"
+                open={isModalOpen}
+                onOk={() =>
+                  postDeleteMutation.mutate({ id: post.id.toString() })
+                }
+                onCancel={handleCancel}
+              >
+                <p>Are you sure you want to delete this post?</p>
+              </Modal>
+            </Form.Item>
+          </div>
+        </Form>
+      </div>
+    ),
+  }))
+
+  if (userLoading || postsLoading) return <div>Loading...</div>
+  if (userError !== null && postsError !== null)
+    return (
+      <div>
+        An error occurred: {userError.toString() || postsError.toString()}
+      </div>
+    )
+
+  return (
+    <div className="flex flex-col gap-8 items-center">
+      <h1 className="text-4xl">User Details</h1>
+      <div className="flex flex-col gap-4 text-lg bg-gray-100 p-5 rounded-lg">
+        <Form
+          onValuesChange={handleFormChange}
+          onFinish={handleFormSubmit}
+          form={form}
+          layout="vertical"
+          size="large"
+        >
+          <div style={{ display: "flex", flexWrap: "wrap", gap: "16px" }}>
+            <Form.Item
+              colon={false}
+              style={{ flex: "1 0 45%" }}
+              label="Name"
+              name="name"
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              style={{ flex: "1 0 45%" }}
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your email!",
+                },
+                {
+                  pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                  message: "Please enter a valid email",
+                },
+              ]}
+              label="Email"
+              name="email"
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item style={{ flex: "1 0 45%" }} label="Phone" name="phone">
+              <Input />
+            </Form.Item>
+            <Form.Item
+              style={{ flex: "1 0 45%" }}
+              label="Website"
+              name="website"
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              style={{ flex: "1 0 45%" }}
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your username!",
+                },
+              ]}
+              label="Username"
+              name="username"
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              style={{ flex: "1 0 45%" }}
+              initialValue={userData.address?.street}
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your address street!",
+                },
+              ]}
+              label="Address Street"
+              name={["street"]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              style={{ flex: "1 0 45%" }}
+              initialValue={userData.address?.suite}
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your address suite!",
+                },
+              ]}
+              label="Address Suite"
+              name={["suite"]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              style={{ flex: "1 0 45%" }}
+              initialValue={userData.address?.city}
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your address city!",
+                },
+              ]}
+              label="Address City"
+              name={["city"]}
+            >
+              <Input />
+            </Form.Item>
+          </div>
+          <div className="flex flex-row gap-6">
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                disabled={!isFormChanged}
+              >
+                Submit
+              </Button>
+            </Form.Item>
+            <Form.Item>
+              <Button
+                onClick={() => {
+                  form.resetFields()
+                  form.setFieldsValue(userData)
+                  setIsFormChanged(false)
+                }}
+                danger
+                disabled={!isFormChanged}
+              >
+                Cancel
+              </Button>
+            </Form.Item>
+          </div>
+        </Form>
+      </div>
+      <h1 className="text-4xl">User Posts</h1>
+      <div className="w-1/2">
+        <Collapse
+          activeKey={activeKey}
+          onChange={handleCollapseChange}
+          accordion
+          items={items}
+        />
+      </div>
+    </div>
+  )
+}
